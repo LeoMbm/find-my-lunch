9fac4cae1193aeb767206cf5b2c35136e5ea6809
LeoMbm <leonidas.j.mbm@gmail.com>
2022-10-28 22:18:47 +0200

    add: middleware requests log

---
+++ app/Middleware/LogRequest.ts
@@ -0,0 +1,12 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class LogRequest {
+  public async handle(
+    { request, response }: HttpContextContract,
+    next: () => Promise<void>
+  ) {
+    const start = process.hrtime.bigint();
+    console.log(`-> ${response.getStatus()} ${request.method()}: ${request.url()} ${Number(process.hrtime.bigint() - start) / 1e6}ms `)
+    await next()
+  }
+}
--- start/kernel.ts
+++ start/kernel.ts
@@ -20,7 +20,10 @@ import Server from '@ioc:Adonis/Core/Server'
 | are defined for every HTTP requests.
 |
 */
-Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser'), 'App/Middleware/SilentAuth'])
+Server.middleware.register([() => import('@ioc:Adonis/Core/BodyParser'),
+                            () => import('App/Middleware/SilentAuth'),
+                            () => import('App/Middleware/LogRequest') 
+                          ])
 
 
 /*
