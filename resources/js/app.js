e0544b61575023db46e04a04bd52ef1d26225cec
LeoMbm <leonidas.j.mbm@gmail.com>
2022-10-31 18:54:08 +0100

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -99,7 +99,7 @@ function addressAutocomplete(containerElement, callback, options) {
   // create input element
   var inputElement = document.createElement("input");
   inputElement.setAttribute("type", "text");
-  inputElement.setAttribute("class", "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500");
+  inputElement.setAttribute("class", "w-full form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500");
   inputElement.setAttribute("placeholder", options.placeholder);
   containerElement.appendChild(inputElement);
 
