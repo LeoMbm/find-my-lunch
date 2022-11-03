3e823535d421f5c799e51d27fa0c96298b743092
LeoMbm <leonidas.j.mbm@gmail.com>
2022-11-03 11:29:47 +0100

    update

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -24,6 +24,7 @@ export default class UsersController {
             
         }       
     }    
+    
     public async login({ request, response, auth }: HttpContextContract){
         const {email, password} = request.only(['email', 'password'])
         try {
